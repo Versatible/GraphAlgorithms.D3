363ff82e1308c76625814917b5bad5221fd5fea8
Xavier Schott <assembla@thegothicparty.com>
2018-05-11 16:51:37 -0700

    Introducing GraphAlgorithms.D3

---
+++ GraphAlgorithms.D3.test/tests/D3ForceDirectedGraphTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace GraphAlgorithms.D3.test
+{
+    using CharNode = WeightedNode<Char>;
+
+    [TestFixture()]
+    public class D3ForceDirectedGraphTest
+    {
+        private WeightedGraph<Char> TestCharGraph()
+        {
+            var graph = new WeightedGraph<Char>();
+
+            var a = new CharNode('A');
+            var b = new CharNode('B');
+
+            graph.AddNode(a);
+            graph.AddNode(b);
+
+            graph.AddUndirectedEdge(a, b, 10);
+
+            return graph;
+        }
+
+        [Test()]
+        public void TestCase()
+        {
+            var d3 = new D3ForceDirectedGraph<Char>(TestCharGraph(),
+                                       (character) => { return character.ToString(); }
+                                      );
+            string expected = "{\"nodes\":[{\"id\":\"A\",\"groupColor\":1},{\"id\":\"B\",\"groupColor\":1}],\"links\":[{\"source\":\"A\",\"target\":\"B\",\"value\":10}]}";
+            string actual = JsonConvert.SerializeObject(d3);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ GraphAlgorithms.D3/sources/D3ForceDirectedGraph.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphAlgorithms.D3
+{
+    public struct D3Node
+    {
+        public readonly string id;
+        public readonly int groupColor;
+        public D3Node(string id, int groupColor = 1) {
+            this.id = id;
+            this.groupColor = groupColor;
+        }
+    }
+
+    public struct D3Link
+    {
+        public readonly string source;
+        public readonly string target;
+        public readonly int value;
+        public D3Link(string source, string target, int value) {
+            this.source = source;
+            this.target = target;
+            this.value = value;
+        }
+    }
+
+    public struct D3ForceDirectedGraph<CONTENT>
+    {
+        public readonly D3Node[] nodes;
+        public readonly D3Link[] links;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:D3Manager.D3Graph`1"/> struct.
+        /// </summary>
+        /// <param name="graph">Graph.</param>
+        /// <param name="converter">Transforms the information in the WeightedNode into </param>
+        public D3ForceDirectedGraph(WeightedGraph<CONTENT> graph, Func<CONTENT, string> converter)
+        {
+            var nList = new List<D3Node>();
+            graph.BFS(node => nList.Add(new D3Node(id: converter(node.Content))));
+
+            var lList = new List<D3Link>();
+            var edges = graph.UndirectedEdges();
+
+            foreach (var edge in edges)
+            {
+                lList.Add(new D3Link(converter(edge.Item1.Content), converter(edge.Item2.Content), edge.Item3));
+            }
+            nodes = nList.ToArray();
+            links = lList.ToArray();
+        }
+    }
+}
